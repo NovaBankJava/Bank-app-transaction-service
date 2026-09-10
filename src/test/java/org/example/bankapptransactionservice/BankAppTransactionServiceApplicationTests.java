2a8a0cb016a14ab216fd02eca1da3078534bdebe
G. Sales <gustavosales0809@gmail.com>
2026-09-10 13:54:19 -0300

    first commit

---
+++ src/test/java/org/example/bankapptransactionservice/BankAppTransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bankapptransactionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppTransactionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
